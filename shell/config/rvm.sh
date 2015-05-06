97fdc86fd5db2de2e9ad2bbc313b774af53a590e
Lewis Eason <me@lewiseason.co.uk>
2015-05-06 15:46:04 +0100

    Loading local AND system rvm's is a Very Bad Idea

--- shell/config/rvm.sh
+++ shell/config/rvm.sh
@@ -1,7 +1,7 @@
-# Load if it exists
-# Local
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-# Systemwide
-[[ -s "/usr/local/rvm/bin/rvm" ]] && . "/usr/local/rvm/bin/rvm"
-
+if [ -x "$HOME/.rvm/scripts/rvm" ]; then
+  # Local
+  source "$HOME/.rvm/scripts/rvm"
+else
+  # Systemwide - rvm doesn't like loading both
+  [[ -s "/usr/local/rvm/bin/rvm" ]] && . "/usr/local/rvm/bin/rvm"
+fi
