f67ea80deda6880286d5b87e6499508d9aeb22f2
Lewis Eason <me@lewiseason.co.uk>
2015-09-03 22:25:32 +0100

    Don't launch keychain unless we're in a shell in a shell

--- shell/config/keychain.sh
+++ shell/config/keychain.sh
@@ -1,3 +1,5 @@
-if [[ -x /usr/bin/keychain ]]; then
-  eval $(/usr/bin/keychain --eval --agents ssh,gpg -Q --quiet id_rsa)
+if [[ -x "/usr/bin/keychain" ]]; then
+  if [[ "$SHLVL" > 1 ]]; then
+    eval `keychain --eval --quiet --agents ssh id_rsa`
+  fi
 fi
