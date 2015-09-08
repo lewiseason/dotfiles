ac2c436f83054439ba430f5c090cb1dd6bfa1f86
Lewis Eason <me@lewiseason.co.uk>
2015-09-08 12:26:17 +0100

    Load bash completion after defining functions, if required

--- shell/bash/completion.sh
+++ shell/bash/completion.sh
@@ -1,7 +1,24 @@
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-  # Enable programmable completion features, just in case it isn't
+  # Enable bash completion
   . /etc/bash_completion
-elif [ -d /etc/bash_completion.d ]; then
-  # Sometimes the file doesn't exist, but the directory does
+fi
+
+type _have 2>/dev/null
+if [ "$?" -ne "0" ]; then
+  # The _have completion function was not defined
+  _have() {
+    type $1 &>/dev/null
+  }
+  #declare -f _have
+
+  have() {
+    unset -v have
+    _have $1 && have=yes
+  }
+  #declare -f have
+fi
+
+if [ -d /etc/bash_completion.d ]; then
+  # Load /etc/bash_completion.d, as it isn't always done automatically
   for file in /etc/bash_completion.d/*; do . "$file"; done
 fi
