e00d2550fe84bb60814c9a95ff0e540be596c975
Lewis Eason <me@lewiseason.co.uk>
2015-09-14 12:19:55 +0100

    Actually load key... that must've fallen off at some point

--- shell/config/keychain.sh
+++ shell/config/keychain.sh
@@ -1,5 +1,5 @@
 if [ -x "/usr/bin/keychain" ] && [ "$SHLVL" -gt "2" ]; then
-  eval $(keychain --eval --quiet)
+  eval $(keychain --eval --quiet id_rsa)
 
   if [ -z "pgrep -u $USER -f gpg-agent" ]; then
     keychain --quiet --agents gpg
