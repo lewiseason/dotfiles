626afefe08b2ae93e62bae3b3231dc4b7d80c2d1
Lewis Eason <me@lewiseason.co.uk>
2016-04-30 01:43:08 +0100

    Always zgen reset when config is dirty

--- shell/fresh/functions.sh
+++ shell/fresh/functions.sh
@@ -2,6 +2,13 @@ conditionally_clear_zgen_cache() {
   ZGEN_CACHE=$(stat -c %Y ~/.zgen/init.zsh 2>/dev/null || echo 0)
   ZGEN_CONFIG=$(git log -1 --format="%at" -- shell/zsh/zgen.sh)
   ZGEN_CONFIG=${ZGEN_CONFIG:-0}
+  ZGEN_DIRTY=$(git status --porcelain -- shell/zsh/zgen.sh | wc -l)
+
+  # If the zgen file in the repo is dirty, always invalidate the cache
+  # Do this by setting the cache timestamp
+  if [ "$ZGEN_DIRTY" -gt "0" ]; then
+    ZGEN_CACHE=-1
+  fi
 
   # If the zgen file in the repo is newer than the one in ~/.zgen - trigger a reset
   # TODO: Doesn't help for development. Maybe an "or file is dirty" check
