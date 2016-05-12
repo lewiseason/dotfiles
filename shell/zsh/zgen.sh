b2cc033a5ba34a5d032e27ef49ebe98cc55d8e82
Lewis Eason <me@lewiseason.co.uk>
2016-05-12 19:57:25 +0100

    Switch themes

--- shell/zsh/zgen.sh
+++ shell/zsh/zgen.sh
@@ -9,9 +9,9 @@ if ! zgen saved; then
   # zgen oh-my-zsh themes/arrow
   # zgen oh-my-zsh themes/flazz
   # zgen oh-my-zsh themes/gianu
-  # zgen oh-my-zsh themes/lukerandall
+  zgen oh-my-zsh themes/lukerandall
   # zgen oh-my-zsh themes/minimal
-  zgen oh-my-zsh themes/robbyrussell
+  #zgen oh-my-zsh themes/robbyrussell
 
   zgen save
 fi
