bba6000aa3f23584ae3668a087dca5b7faa735b3
Lewis Eason <me@lewiseason.co.uk>
2016-08-22 21:45:59 +0100

    zsh: Tweak plugin loading to try and improve performance

--- shell/zsh/zgen.sh
+++ shell/zsh/zgen.sh
@@ -1,17 +1,19 @@
 if ! zgen saved; then
-  zgen oh-my-zsh oh-my-zsh.sh
+  zgen load seletskiy/zsh-zgen-compinit-tweak
+
+  zgen oh-my-zsh
+
+  zgen oh-my-zsh lib/completion.zsh
+  zgen oh-my-zsh lib/git.zsh
+  zgen oh-my-zsh lib/history.zsh
+  zgen oh-my-zsh lib/key-bindings.zsh
 
   zgen oh-my-zsh plugins/git
 
   zgen load zsh-users/zsh-completions src
   zgen load horosgrisa/autoenv
 
-  # zgen oh-my-zsh themes/arrow
-  # zgen oh-my-zsh themes/flazz
-  # zgen oh-my-zsh themes/gianu
-  zgen oh-my-zsh themes/lukerandall
-  # zgen oh-my-zsh themes/minimal
-  #zgen oh-my-zsh themes/robbyrussell
+  zgen oh-my-zsh themes/jreese
 
   zgen save
 fi
