4a3a06bb001299f6754c6a3a5b2508e37a8771c4
Lewis Eason <me@lewiseason.co.uk>
2018-03-06 21:50:31 +0000

    Discard the error output of the keychain command - it's rarely useful.

--- shell/config/keychain.sh
+++ shell/config/keychain.sh
@@ -1,3 +1,3 @@
 if [ -x "/usr/bin/keychain" ] && [ "$SHLVL" -gt "2" ]; then
-  eval $(keychain --agents ssh,gpg --eval --quick --quiet id_rsa)
+  eval $(keychain --agents ssh,gpg --eval --quick --quiet id_rsa 2>/dev/null)
 fi
