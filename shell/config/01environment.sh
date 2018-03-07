2f44ea8b53a4f4b74da2de40254a694148033d31
Lewis Eason <me@lewiseason.co.uk>
2018-03-07 16:47:34 +0000

    Tweak gitconfig and add aliases for identity switching

--- shell/config/01environment.sh
+++ shell/config/01environment.sh
@@ -3,6 +3,7 @@ VISUAL='nvim'
 PAGER='less -SFXiMR'
 BROWSER='firefox:google-chrome'
 MAILTO=me@lewiseason.co.uk
+MAILTO_WORK=lewis.eason@commsworld.com
 
 ACK_PAGER="$PAGER"
 
@@ -14,6 +15,7 @@ AUTOSSH_POLL=10
 pathPrepend ~/bin
 pathAppend node_modules/.bin
 
-export EDITOR VISUAL PAGER BROWSER LANG PATH LS_COLORS AUTOSSH_POLL ACK_PAGER
+export EDITOR VISUAL PAGER BROWSER MAILTO MAILTO_WORK
+export LANG PATH LS_COLORS AUTOSSH_POLL ACK_PAGER
 
 alias vim=nvim
