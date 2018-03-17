d30b1c482e8b357bcaf164fec0e758b46a4b044d
Lewis Eason <me@lewiseason.co.uk>
2018-03-17 19:44:17 +0000

    Dont lazy-load rvm

--- shell/config/rvm.sh
+++ shell/config/rvm.sh
@@ -1,13 +1,8 @@
-source_rvm() {
-  if [ -x "$HOME/.rvm/scripts/rvm" ]; then
-    source "$HOME/.rvm/scripts/rvm"
-  elif [ -x "/usr/local/rvm/bin/rvm" ]; then
-    source "/usr/local/rvm/bin/rvm"
-  fi
-}
+if [ -x "$HOME/.rvm/scripts/rvm" ]
+then
+  source "$HOME/.rvm/scripts/rvm"
 
-rvm() {
-  unset -f rvm
-  source_rvm
-  rvm "$@"
-}
+elif [ -x "/usr/local/rvm/bin/rvm" ]
+then
+  source "/usr/local/rvm/bin/rvm"
+fi
